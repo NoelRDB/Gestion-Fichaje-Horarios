7b01c1650bb3320f9079f018809f6c13a76c26c5
Noel Roman <noelgijon98@gmail.com>
2025-12-01 09:44:45 +0100

    Add dependences

---
+++ gestion-fichajes/src/main/java/com/noelrdb/gestion_fichajes/GestionFichajesApplication.java
@@ -0,0 +1,13 @@
+package com.noelrdb.gestion_fichajes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionFichajesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionFichajesApplication.class, args);
+	}
+
+}
---
+++ gestion-fichajes/src/test/java/com/noelrdb/gestion_fichajes/GestionFichajesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noelrdb.gestion_fichajes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionFichajesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
